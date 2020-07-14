0ecbcaf3f4eec2e427dc1ee5bda84a61f4c9ac7e
Harshit Singh <singhsnf@amazon.com>
2020-07-14 23:10:35 +0530

    Updated code to Pratyush

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Hello World
+          Hello World @Pratsy !
         </p>
         <a
           className="App-link"
